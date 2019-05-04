61642cff9ed5249827ca9104b1478ae60546a256
Jasjit Singh Marwah <jasjitsm@gmail.com>
2019-05-04 16:25:29 +0530

    Sets up the basic architecture of the application
    -- Created and setup .env file for absolute imports.
    -- Installed node-sass and axios dependencies.
    -- Setup base folder structure and barrel files.

---
+++ src/components/App/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      {/*
+      <Sidebar>
+        Logo
+        Navigation
+      </Sidebar>
+
+      <Content>
+        Router for Pages
+      </Content>
+      
+      <Player>
+        Controls
+        Settings
+      </Player> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Discover/Discover.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Discover.scss';
+
+function Discover() {
+  return (
+    <div className="Discover">
+      Hello World!
+    </div>
+  );
+}
+
+export default Discover;
